c8cf80379b6bfe54f9e147cbf79bd0adfccc80c3
KakugaGoka <kakugagoka@live.com>
2019-12-18 12:54:14 -0800

    Initial Commit

---
+++ Assets/Scripts/PokedexController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PokedexController : MonoBehaviour
+{
+    public GameObject pokedexPrefab;
+    public GameObject contentPanel;
+
+    private void Start()
+    {
+        // Load Pokemon From JSON and apply them to the entry prefab
+        string pokemonString = File.ReadAllText(Path.Combine(Application.dataPath, "JSON/Pokemon.json"));
+        Pokemon[] pokedexEntries = JsonHelper.FromJson<Pokemon>(pokemonString);
+        Debug.Log(pokedexEntries.Length);
+        pokedexEntries = pokedexEntries.OrderBy(x => x.number).ToArray<Pokemon>();
+
+        // Get the name field so to set a default pokemon to it. 
+        Text nameField = GameObject.Find("Name Field").GetComponent<Text>();
+
+        foreach (Pokemon pokemon in pokedexEntries)
+        {
+#if DEBUG
+            string filePath = "Icons/" + pokemon.image;
+            string dataPath = Application.dataPath;
+            string combinedPath = Path.Combine(dataPath, filePath + ".png");
+            if (!File.Exists(combinedPath))
+            {
+                Debug.LogError("Sprite was not found: " + combinedPath);
+            }
+#endif
+            GameObject newPokemon = Instantiate(pokedexPrefab) as GameObject;
+            PokedexEntry controller = newPokemon.GetComponent<PokedexEntry>();
+            controller.pokemon = pokemon;
+            controller.species.text = pokemon.number + " - " + pokemon.species;
+            newPokemon.transform.SetParent(contentPanel.transform);
+            newPokemon.transform.localScale = Vector3.one;
+            // Set default if there isn't already one
+            if (nameField.text == "")
+            {
+                controller.OnSelected();
+            }
+        }
+
+
+
+        string jsonString = File.ReadAllText(Path.Combine(Application.dataPath, "JSON/PokemonTypes.json"));
+        PokemonType[] pokemonTypes = JsonHelper.FromJson<PokemonType>(jsonString);
+        Debug.Log(pokemonTypes.Length);
+    }
+}
---
+++ Assets/Scripts/Pokemon.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class Pokemon
+{
+    public Sprite
+        sprite;
+
+    public string 
+        image,
+        species, 
+        region,
+        type,
+        size,
+        weight,
+        gender,
+        egg,
+        hatch,
+        diet,
+        habitat,
+        entry;
+
+    public int  
+        number,
+        hp,
+        atk,
+        def,
+        spatk,
+        spdef,
+        spd,
+        stage,
+        athleticsDie,
+        acrobaticsDie,
+        combatDie,
+        stealthDie,
+        perceptionDie,
+        focusDie,
+        techEduDie,
+        athleticsBonus,
+        acrobaticsBonus,
+        combatBonus,
+        stealthBonus,
+        perceptionBonus,
+        focusBonus,
+        techEduBonus;
+
+    public bool
+        legendary;
+
+    public string[]
+        evolutions,
+        capabilities,
+        basicAbilites,
+        advancedAbilities,
+        highAbilities,
+        moves; 
+
+    public Pokemon(string _image,
+            int _number,
+            int _hp,
+            int _atk,
+            int _def,
+            int _spatk,
+            int _spdef,
+            int _spd,
+            int _stage,
+            int _athleticsDie,
+            int _acrobaticsDie,
+            int _combatDie,
+            int _stealthDie,
+            int _perceptionDie,
+            int _focusDie,
+            int _techEduDie,
+            int _athleticsBonus,
+            int _acrobaticsBonus,
+            int _combatBonus,
+            int _stealthBonus,
+            int _perceptionBonus,
+            int _focusBonus,
+            int _techEduBonus,
+            string _species,
+            string _region,
+            string _type,
+            string _size,
+            string _weight,
+            string _gender,
+            string _egg,
+            string _hatch,
+            string _diet,
+            string _habitat,
+            string _entry,
+            bool _legendary,
+            string[] _evolutions,
+            string[] _capabilities,
+            string[] _basicAbilites,
+            string[] _advancedAbilites,
+            string[] _highAbilities,
+            string[] _moves
+        )
+    {
+        image = _image;
+        number = _number;
+        hp = _hp;
+        atk = _atk;
+        def = _def;
+        spatk = _spatk;
+        spdef = _spdef;
+        spd = _spd;
+        stage = _stage;
+        species = _species;
+        region = _region;
+        type = _type;
+        size = _size;
+        weight = _weight;
+        gender = _gender;
+        egg = _egg;
+        hatch = _hatch;
+        diet = _diet;
+        habitat = _habitat;
+        entry = _entry;
+        legendary = _legendary;
+        evolutions = _evolutions;
+        capabilities = _capabilities;
+        moves = _moves;
+        basicAbilites = _basicAbilites;
+        advancedAbilities = _advancedAbilites;
+        highAbilities = _highAbilities;
+        athleticsDie = _athleticsDie;
+        acrobaticsDie = _acrobaticsDie;
+        combatDie = _combatDie;
+        stealthDie = _stealthDie;
+        perceptionDie = _perceptionDie;
+        focusDie = _focusDie;
+        techEduDie = _techEduDie;
+        athleticsBonus = _athleticsBonus;
+        acrobaticsBonus = _acrobaticsBonus;
+        combatBonus = _combatBonus;
+        stealthBonus = _stealthBonus;
+        perceptionBonus = _perceptionBonus;
+        focusBonus = _focusBonus;
+        techEduBonus = _techEduBonus;
+    }
+}
